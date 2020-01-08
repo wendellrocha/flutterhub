991597bca69e8f01344c41bfa72f73f643dd1523
Wendell Rocha <wendellrocha@outlook.com>
2020-01-08 08:49:59 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    title: 'Github with GraphQL',
+    debugShowCheckedModeBanner: false,
+    home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
